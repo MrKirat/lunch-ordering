4bdf67f6347dcc508cf1274b966914298d993e26
Taras Viiatyk <tviyatyk@gmail.com>
2020-06-20 22:00:15 +0300

    Add Order model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_18_222448) do
+ActiveRecord::Schema.define(version: 2020_06_20_172148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,13 @@ ActiveRecord::Schema.define(version: 2020_06_18_222448) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -64,4 +71,5 @@ ActiveRecord::Schema.define(version: 2020_06_18_222448) do
 
   add_foreign_key "food_categories", "food_menus"
   add_foreign_key "food_items", "food_categories"
+  add_foreign_key "orders", "users"
 end
