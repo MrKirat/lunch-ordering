d0fc220a21f7f09daa595f7f4bb7f45616a1e34d
Taras Viiatyk <tviyatyk@gmail.com>
2020-07-04 20:50:58 +0300

    Fix API base_controller

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::BaseController < ApplicationController
-  extend Roleble::Controller
+  include Roleble::Controller
   include DeviseTokenAuth::Concerns::SetUserByToken
   before_action :authenticate_admin!
   before_action { check_role current_admin, :api }
