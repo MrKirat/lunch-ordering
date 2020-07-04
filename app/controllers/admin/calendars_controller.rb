2ed306dc44d9d532f4e690f42555b1450280c1fd
Taras Viiatyk <tviyatyk@gmail.com>
2020-07-05 01:44:05 +0300

    Add Calendarable concern

--- app/controllers/admin/calendars_controller.rb
+++ app/controllers/admin/calendars_controller.rb
@@ -1,6 +1,8 @@
 class Admin::CalendarsController < Admin::BaseController
-  before_action :set_menus, only: [:day ,:month]
-  before_action :set_orders, only: [:day ,:month]
+  include Calendarable
+  prepare_time_point actions: [:day, :month], param_name: :start_date, fallback_location: :admin_calendars_month_path
+  before_action :set_menus, only: [:day, :month]
+  before_action :set_orders, only: [:day, :month]
 
   # GET /admin/calendars/day
   def day
@@ -14,15 +16,10 @@ class Admin::CalendarsController < Admin::BaseController
   private
 
   def set_menus
-    @menus = Food::Menu.created_within(time_point, params[:action])
+    @menus = Food::Menu.created_within(@time_point, params[:action])
   end
 
   def set_orders
-    @orders = Order.created_within(time_point, params[:action]).includes(:food_items)
-  end
-
-  def time_point
-    return Time.zone.parse(params[:start_date]) if params[:start_date].present?
-    Time.current
+    @orders = Order.created_within(@time_point, params[:action]).includes(:food_items)
   end
 end
--- app/controllers/user/dashboard_controller.rb
+++ app/controllers/user/dashboard_controller.rb
@@ -1,7 +1,9 @@
 class User::DashboardController < User::BaseController
+  include Calendarable
+  prepare_time_point actions: [:show], param_name: :start_date, fallback_location: :dashboard_path
 
   # GET /dashboard
   def show
-    @menus = Food::Menu.first_per_day_within(Time.current, :week)
+    @menus = Food::Menu.first_per_day_within(@time_point, :week)
   end
 end
