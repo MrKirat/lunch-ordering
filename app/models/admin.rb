651cbe1c6055f5e2294bd8e7653832030f599dc4
Taras Viiatyk <tviyatyk@gmail.com>
2020-07-04 21:09:39 +0300

    Remove recoverable devise module

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,7 +2,7 @@ class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :registerable, :trackable and :omniauthable
   devise :database_authenticatable, :timeoutable,
-         :recoverable, :rememberable, :validatable
+         :rememberable, :validatable
 
   validates :email, presence: true, format: {with: Devise::email_regexp}
   validate :must_have_valid_roles
